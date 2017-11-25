69208a180f5826ed522623bc85c10c4487b4de4b
Jose <codesmash50@gmail.com>
2017-11-25 08:33:23 -0500

    Added Methods to print student and teacher details recieved from user

--- ConsoleAppMod3/Program.cs
+++ ConsoleAppMod3/Program.cs
@@ -10,6 +10,43 @@ namespace ConsoleAppMod3
     {
         static void Main(string[] args)
         {
+            GetStudentInformation();
+        }
+
+        static void GetStudentInformation()
+        {
+            Console.WriteLine("Enter the student's first name: ");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter the student's last name");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter the student's birthday");
+            string birthday = Console.ReadLine();
+
+            PrintStudentDetails(firstName, lastName, birthday);
+        }
+
+        static void PrintStudentDetails(string first, string last, string birthday)
+        {
+            Console.WriteLine("{0} {1} was born on: {2}", first, last, birthday);
+        }
+
+        static void GetTeacherInformation()
+        {
+            // Gets teacher, course, program, and degree
+            Console.WriteLine("Enter the teacher's name");
+            string teacher = Console.ReadLine();
+            Console.WriteLine("Enter the course");
+            string course = Console.ReadLine();
+            Console.WriteLine("Enter the program");
+            string program = Console.ReadLine();
+            Console.WriteLine("Enter the degree");
+            string degree = Console.ReadLine();
+            PrintTecherDetails(teacher, course, program, degree);
+        }
+
+        private static void PrintTecherDetails(string teacher, string course, string program, string degree)
+        {
+            Console.WriteLine("{0} will teach course {1} in the program {2} for degree {3}", teacher, course, program, degree);
         }
     }
 }
