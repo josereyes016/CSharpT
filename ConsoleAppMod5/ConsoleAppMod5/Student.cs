1eee86e94c4dcb9052c1f1790e49ce9f6c4c4d9b
Jose <codesmash50@gmail.com>
2017-11-26 13:16:56 -0500

    Completed Student class

--- ConsoleAppMod5/ConsoleAppMod5/Student.cs
+++ ConsoleAppMod5/ConsoleAppMod5/Student.cs
@@ -8,6 +8,22 @@ namespace ConsoleAppMod5
 {
     class Student
     {
+        private string firstName;
+        private string lastName;
+        private DateTime birthday;
+        public static int objectCount = 0;
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthday { get; set; }
+
+        public Student( string firstname, string lastName, DateTime birthday)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.birthday = birthday;
+            objectCount++;
+        }
 
 
     }
